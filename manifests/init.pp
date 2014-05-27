c5acd5ceb9478a126982e0ca5bbe3515ba82ce49
Bjarke Walling <bwp@bwp.dk>
2014-05-27 19:19:17 +0200

    Removed unnecessary name.
    It just complicates matters, when you look at the lock files in /var/db/. With this change the lock file is correctly named `.puppet_pkgdmg_installed_GPGTools`. Including a name like 'Install.pkg' is also confusing to other people, since you could get the idea that it only runs the Install.pkg inside the downloaded .dmg, which is not the case. The `pkgdmg` puppet provider installs all the .pkgs inside the given .dmg file. It so happens that the installer is a .pkg file and the uninstaller is a .app file, so it works out in the end.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,6 @@
 # Install GPGTools.
 class gpgtools {
   package { 'GPGTools':
-    name     => 'Install.pkg',
     provider => 'pkgdmg',
     source   => 'https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg',
   }

67326faee05c538ac0a1d0cb68fda3365e9e0c5f
Richard Leland <rich@richards-air.home>
2014-05-31 15:14:22 -0400

    it appears the name was necessary

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 # Install GPGTools.
 class gpgtools {
   package { 'GPGTools':
+    name     => 'Install.dmg',
     provider => 'pkgdmg',
     source   => 'https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg',
   }
